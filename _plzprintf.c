837835b17b215d1a88e316cd14d3faae39eecbdb
wdmd2022 <doug@dougrules.com>
2022-07-01 14:41:05 -0700

    upload putchar and draft of new printf

---
+++ _plzprintf.c
@@ -0,0 +1,20 @@
+/**
+ * _plzprintf - prints anything at all
+ *
+ * @format: format string
+ *
+ * Return: how many bytes it prints to standard output
+ */
+int _plzprintf(char *format, ...)
+{
+	va_list args;
+	unsigned int index = 0;
+	unsigned int returnlength = 0;
+
+	va_start (args, format);
+
+	while (format[index])
+	{
+
+	}
+}
