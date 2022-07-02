fb6979783c0926fd05800eb037246846385ad1a4
Vernon-444 <mt.vernon44@gmail.com>
2022-07-01 18:06:15 -0700

    tested and ready to creat funcs

---
+++ _putint.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putint - prints integers fed int from mods d & i
+ *
+ * @n: num fed from printf
+ *
+ * Return: int of num of chars written
+ */
+
+int _putint(int n)
+{
+        
+}
